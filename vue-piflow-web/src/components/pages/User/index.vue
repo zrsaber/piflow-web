8246f5dbf93098ed412c85013c8cf6902653c47a
zrq <529978097@qq.com>
2021-08-01 13:12:50 +0800

    暂时提交

--- vue-piflow-web/src/components/pages/User/index.vue
+++ vue-piflow-web/src/components/pages/User/index.vue
@@ -5,12 +5,16 @@
             <div class="left">
                 <span>{{$t("sidebar.user")}}</span>
             </div>
+
+            <!-- 添加用户的按钮建立 -->
             <div class="right">
                 <span class="button-warp" @click="handleModalSwitch">
                     <Icon type="md-add" />
                 </span>
             </div>
         </div>
+
+        <!-- 搜索栏的建立，此处的搜索栏与系统的其他地方相同 -->
         <div class="input">
             <Input
                 suffix="ios-search"
@@ -19,8 +23,12 @@
                 style="width: 300px"
             />
         </div>
+
+        <!-- 建立主体：表单 -->
         <Table border :columns="columns" :data="tableData">
         </Table>
+
+        <!-- 下拉页面的建立 -->
         <div class="page">
             <Page
                 :prev-text="$t('page.prev_text')"
@@ -33,10 +41,30 @@
                 @on-page-size-change="onPageSizeChange"
             />
         </div>
+
+        <!-- 点击添加按钮出现的 -->
     </section>
 </template>
 
 <script>
+export default {
+    name:"user",
+    components:{},
+    data() {
+        return{
+            isOpen :false,
+            page:1,
+            limit:10,
+            total:0,
+            tableData:[],
+            param:"",
+
+            row:null,
+
+            //下面是表格的初始化
+        }
+    }
+}
 </script>
 <style lang="scss" scoped>
 @import "./index.scss";
